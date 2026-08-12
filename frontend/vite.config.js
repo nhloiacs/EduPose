4c8bc2c9c75423f5430410f3e3abda103e097bc7
hrry6 <harrysiloam06@gmail.com>
2026-08-12 22:55:36 +0700

    fix: docker for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: '/api',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
